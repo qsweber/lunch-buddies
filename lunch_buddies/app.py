b48669b4977e1f1cc746e9751745f499a6697887
quinn <quinn@quinnweber.com>
2018-01-09 21:02:28 -0800

    travis deploy instead of after_success

--- lunch_buddies/app.py
+++ lunch_buddies/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/api/v0/poll', methods=['POST', 'GET'])
 def index():
     response = jsonify({
-        'foo': 1,
+        'foo': 2,
         'bar': 2,
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
---
+++ lunch_buddies/scripts/deploy.sh
@@ -0,0 +1,4 @@
+coveralls
+pip install -r requirements.txt
+pip install zappa
+zappa update production
