b1f52761ed5797215dcc9065ceb0dc89d20cf219
quinn <quinn@quinnweber.com>
2018-01-18 09:02:25 -0800

    add endpoint for closing poll

--- lunch_buddies/app.py
+++ lunch_buddies/app.py
@@ -3,8 +3,9 @@ import json
 
 from flask import Flask, jsonify, request
 
-from lunch_buddies.actions.listen_to_poll import listen_to_poll as listen_to_poll_action
 from lunch_buddies.actions.create_poll import create_poll as create_poll_action
+from lunch_buddies.actions.close_poll import close_poll as close_poll_action
+from lunch_buddies.actions.listen_to_poll import listen_to_poll as listen_to_poll_action
 from lunch_buddies.clients.slack import SlackClient
 
 app = Flask(__name__)
@@ -37,6 +38,24 @@ def create_poll():
     slack_client = SlackClient()
 
     outgoing_message_payload = create_poll_action(request_payload, slack_client)
+
+    response = jsonify(outgoing_message_payload)
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+
+@app.route('/api/v0/poll/close', methods=['POST'])
+def close_poll():
+    '''
+    Close a poll
+    '''
+    request_payload = json.loads(json.dumps(request.form))
+
+    slack_client = SlackClient()
+
+    outgoing_message_payload = close_poll_action(request_payload, slack_client)
 
     response = jsonify(outgoing_message_payload)
 
