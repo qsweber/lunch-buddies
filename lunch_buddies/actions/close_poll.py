be4ccb775719246a361cc897a50bbcfa38f599a7
quinn <quinn@quinnweber.com>
2018-01-31 17:57:54 -0800

    fix bug in grouping responses

--- lunch_buddies/actions/close_poll.py
+++ lunch_buddies/actions/close_poll.py
@@ -1,4 +1,4 @@
-from itertools import groupby
+from collections import defaultdict
 import random
 
 from lunch_buddies.constants.queues import GROUPS_TO_NOTIFY, GroupsToNotifyMessage
@@ -10,15 +10,8 @@ def close_poll(message, slack_client, sqs_client, polls_dao, poll_responses_dao)
 
     poll_responses = poll_responses_dao.read('callback_id', str(poll.callback_id))
 
-    # randomly group the responding users
-    poll_responses_by_response = {
-        response: list(items)
-        for response, items in groupby(
-            poll_responses,
-            key=lambda item: item.response,
-        )
-        if 'yes' in response
-    }
+    poll_responses_by_response = _group_by_answer(poll_responses)
+
     for answer, messages in poll_responses_by_response.items():
         for group in get_groups(messages, 7, 5):
             user_ids = [poll_response.user_id for poll_response in group]
@@ -28,6 +21,15 @@ def close_poll(message, slack_client, sqs_client, polls_dao, poll_responses_dao)
             )
 
     return True
+
+
+def _group_by_answer(poll_responses):
+    poll_responses_by_response = defaultdict(list)
+    for poll_response in poll_responses:
+        if 'yes' in poll_response.response:
+            poll_responses_by_response[poll_response.response].append(poll_response)
+
+    return dict(poll_responses_by_response)
 
 
 def get_groups(elements, group_size, smallest_group):
--- lunch_buddies/tests/actions/test_close_poll.py
+++ lunch_buddies/tests/actions/test_close_poll.py
@@ -1,7 +1,10 @@
+import datetime
 import random
+from uuid import UUID
 
 import pytest
 
+from lunch_buddies.models.poll_responses import PollResponse
 import lunch_buddies.actions.close_poll as module
 
 
@@ -18,5 +21,28 @@ def test_get_groups(elements, group_size, smallest_group, expected):
     random.seed(0)
 
     actual = module.get_groups(elements, group_size, smallest_group)
+
+    assert actual == expected
+
+
+def test_group_by_answer():
+    poll_responses = [
+        PollResponse(callback_id=UUID('450c4b9d-267b-431e-97b9-e3cb32d233c4'), user_id='U3LTPT61J', created_at=datetime.datetime(2018, 1, 31, 16, 43, 19, 93329), response='yes_1230'),
+        PollResponse(callback_id=UUID('450c4b9d-267b-431e-97b9-e3cb32d233c4'), user_id='U69J2B8E8', created_at=datetime.datetime(2018, 1, 31, 16, 43, 5, 494358), response='yes_1145'),
+        PollResponse(callback_id=UUID('450c4b9d-267b-431e-97b9-e3cb32d233c4'), user_id='U71ECE86R', created_at=datetime.datetime(2018, 1, 31, 16, 43, 26, 9092), response='no'),
+        PollResponse(callback_id=UUID('450c4b9d-267b-431e-97b9-e3cb32d233c4'), user_id='U799LRKSA', created_at=datetime.datetime(2018, 1, 31, 16, 43, 36, 221596), response='yes_1145'),
+    ]
+
+    expected = {
+        'yes_1145': [
+            PollResponse(callback_id=UUID('450c4b9d-267b-431e-97b9-e3cb32d233c4'), user_id='U69J2B8E8', created_at=datetime.datetime(2018, 1, 31, 16, 43, 5, 494358), response='yes_1145'),
+            PollResponse(callback_id=UUID('450c4b9d-267b-431e-97b9-e3cb32d233c4'), user_id='U799LRKSA', created_at=datetime.datetime(2018, 1, 31, 16, 43, 36, 221596), response='yes_1145'),
+        ],
+        'yes_1230': [
+            PollResponse(callback_id=UUID('450c4b9d-267b-431e-97b9-e3cb32d233c4'), user_id='U3LTPT61J', created_at=datetime.datetime(2018, 1, 31, 16, 43, 19, 93329), response='yes_1230'),
+        ],
+    }
+
+    actual = module._group_by_answer(poll_responses)
 
     assert actual == expected
