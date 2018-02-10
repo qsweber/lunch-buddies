698ac45d90f607d14f4092b1f8584db15c5e40f7
quinn <quinn@quinnweber.com>
2018-02-10 01:57:25 -0800

    use the correct token

--- lunch_buddies/clients/slack.py
+++ lunch_buddies/clients/slack.py
@@ -6,11 +6,11 @@ class ChannelDoesNotExist(Exception):
 
 
 class SlackClient(object):
-    def _get_base_client_for_team(self, team):
-        return BaseSlackClient(team.bot_access_token)
+    def _get_base_client_for_team(self, token):
+        return BaseSlackClient(token)
 
     def create_channel(self, team, name, is_private, **kwargs):
-        return self._get_base_client_for_team(team).api_call(
+        return self._get_base_client_for_team(team.access_token).api_call(
             'conversations.create',
             name=name,
             is_private=is_private,
@@ -18,19 +18,19 @@ class SlackClient(object):
         )
 
     def open_conversation(self, team, **kwargs):
-        return self._get_base_client_for_team(team).api_call('conversations.open', **kwargs)
+        return self._get_base_client_for_team(team.bot_access_token).api_call('conversations.open', **kwargs)
 
     def post_message(self, team, **kwargs):
-        return self._get_base_client_for_team(team).api_call('chat.postMessage', **kwargs)
+        return self._get_base_client_for_team(team.bot_access_token).api_call('chat.postMessage', **kwargs)
 
     def _channels_list_internal(self, team):
-        return self._get_base_client_for_team(team).api_call('channels.list')
+        return self._get_base_client_for_team(team.bot_access_token).api_call('channels.list')
 
     def _channels_info_internal(self, team, **kwargs):
-        return self._get_base_client_for_team(team).api_call('channels.info', **kwargs)
+        return self._get_base_client_for_team(team.bot_access_token).api_call('channels.info', **kwargs)
 
     def _users_info_internal(self, team, **kwargs):
-        return self._get_base_client_for_team(team).api_call('users.info', **kwargs)
+        return self._get_base_client_for_team(team.bot_access_token).api_call('users.info', **kwargs)
 
     def get_channel(self, team, name):
         channels = [
