bb3af2d1b6114b3e62d4fb3410feef394f28a0a7
quinn <quinn@quinnweber.com>
2018-02-25 20:39:09 -0800

    update constants

--- lunch_buddies/constants/help.py
+++ lunch_buddies/constants/help.py
@@ -1,13 +1,14 @@
-APP_EXPLANATION = '''
-This is an app to help build relationships between members of a growing team. Each week, users will have the opportunity to join a small group of teammates for lunch. The `@lunch-buddies-bot` takes care of all of the logistics.
+TAG_LINE = 'This app helps build relationships between members of a growing team. Each week, users will have the opportunity to join a small group of teammates for lunch. The @lunch_buddies_bot takes care of all the logistics.'
 
-To initiate the process, simply run the `/lunch_buddies_create` command. This will send a message to each member of the #lunch-buddies channel asking if they want to participate. The possible answers are: "Yes (11:45)", "Yes (12:30)", and "No".
+CREATE_POLL = 'To initiate the process, run the `/lunch_buddies_create` command. This will send a message to each member of the #lunch-buddies channel asking if they want to participate. The default possible answers are: "Yes (12:00)", and "No". You may also specify a set of custom times. For example, "/lunch_buddies_create 1145, 1230" will create a poll whose possible answers are "Yes (11:45)", "Yes (12:30)" and "No". You do not need to explicitly set "no" as an option; it is included by default.'
 
-Once everyone has answered, a user should run the `/lunch_buddies_close` command. This will randomly group people by their answer and start a private group message for each group.
+CLOSE_POLL = 'Once everyone has answered, a user should run the `/lunch_buddies_close` command. This will randomly group people by their answer and start a private group message for each group.'
 
-If you have any questions/comments/concerns/etc, please send an email to lunchbuddies@quinnweber.com
-'''
+SUPPORT_LINE = 'If you have any questions/comments/concerns/etc, please send an email to lunchbuddies@quinnweber.com'
 
-CREATE_POLL = '`/lunch_buddies_create` Poll each member of the #lunch_buddies channel about participation. Once all responses have been received, run the /lunch_buddies_close command'
-
-CLOSE_POLL = '`/lunch_buddies_close` Pull the responses to the latest poll and randomly group users based on their response.'
+APP_EXPLANATION = '{}\n\n{}\n\n{}\n\n{}'.format(
+  TAG_LINE,
+  CREATE_POLL,
+  CLOSE_POLL,
+  SUPPORT_LINE,
+)
