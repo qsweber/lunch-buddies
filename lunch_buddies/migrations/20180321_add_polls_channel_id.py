4f98da468002be4a852e9f5fa7ba0acc6fe33cde
quinn <quinn@quinnweber.com>
2018-03-26 21:27:35 -0700

    add migration

--- lunch_buddies/migrations/20180321_add_polls_channel_id.py
+++ lunch_buddies/migrations/20180321_add_polls_channel_id.py
@@ -1,6 +1,11 @@
+import logging
+
 from lunch_buddies.dao.polls import PollsDao
 from lunch_buddies.dao.teams import TeamsDao
 from lunch_buddies.clients.slack import SlackClient
+
+
+logger = logging.getLogger(__name__)
 
 
 def migrate():
@@ -17,7 +22,16 @@ def migrate():
         team_id = raw_poll['team_id']
         team = teams_dao.read('team_id', team_id)[0]
 
-        channel = slack_client.get_channel(team, 'lunch_buddies')
+        test = slack_client._channels_list_internal(team)
+        if isinstance(test, dict) and 'error' in test:
+            logger.error('Error with team {}: {}'.format(team_id, test['error']))
+            continue
+
+        try:
+            channel = slack_client.get_channel(team, 'lunch_buddies')
+        except Exception as e:
+            logger.error('Error with team {}: {}'.format(team_id, str(e)))
+            continue
 
         dynamo_table.update_item(
             Key={
