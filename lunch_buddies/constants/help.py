ad0edc1a0d49564c30faa0fbb2fb2d60d2b87ab0
quinn <quinn@quinnweber.com>
2018-11-01 20:47:59 -0700

    update constants

--- lunch_buddies/constants/help.py
+++ lunch_buddies/constants/help.py
@@ -1,10 +1,33 @@
-TAG_LINE = 'This app helps build relationships between members of a growing team. Each week, users will have the opportunity to join a small group of teammates for lunch. The `@lunch_buddies_bot` takes care of all the logistics.'
+from lunch_buddies.constants.polls import DEFAULT_GROUP_SIZE
 
-CREATE_POLL = 'To initiate the process, invite @lunch_buddies_bot to a channel and say `@lunch_buddies_bot create`. This will send a message to each member of the current channel asking if they want to participate.\n\nThe default possible answers are: `Yes (12:00)`, and `No`. You may also specify a set of custom times. For example, `@lunch_buddies_bot create 1145, 1230` will create a poll whose possible answers are `Yes (11:45)`, `Yes (12:30)` and `No`. You do not need to explicitly set `no` as an option; it is included by default.\n\nYou may also use the `/lunch_buddies_create` command. This slash command can be invoked from anywhere in slack and it will poll the members of the `#lunch_buddies` channel. This is the legacy way of creating a poll.\n\nIt is possible to have multiple polls going at the same time; they are uniquely defined by the channel from which they were created. For example, you can go to the #seattle_office channel and say `@lunch_buddies_bot create`, and then go right to the #san_francisco_office channel and say `@lunch_buddies_bot create`. Members of each channel will be independently polled and grouped.'
+TAG_LINE_SHORT = 'Meet random teammates for lunch'
 
-CLOSE_POLL = 'Once everyone has answered, you should go back to the same channel from which the poll was created and write `@lunch_buddies_bot close`. This will randomly group the responders by their answer and start a private group message for each group. For example, if 60 people said `Yes (11:45)` and 30 people said `Yes (12:30)`, @lunch_buddies_bot will create 10 random groups for the 11:45 time and 5 random groups for the 12:30 time.\n\nIf you created the poll with the slack command `/lunch_buddies_create`, you should close it with the slash command `/lunch_buddies_close`.'
+TAG_LINE = 'Builds relationships between teammates by coordinating random lunch groups.'
 
-SUPPORT_LINE = 'If you have any questions/comments/concerns/etc, please send an email to lunchbuddies@quinnweber.com'
+CREATE_POLL = (
+  'To initiate the process, invite the Lunch Buddies bot to any channel and ' +
+  'say `@Lunch Buddies create`. This will send a message to each member of the current channel ' +
+  'asking if they want to participate.' +
+  '\n\n' +
+  'The default options are: `Yes (12:00)`, and `No`, but you can customize by saying `@Lunch Buddies create 1145, 1230`.' +
+  '\n\n' +
+  'The default group size is {}, but this can be customize by saying `@Lunch Buddies create 1145, 1230 size=4`'.format(DEFAULT_GROUP_SIZE) +
+  '\n\n' +
+  'You can also create polls using the legacy `/lunch_buddies_create` slash command. ' +
+  'Regardless of where this command is invoked, it will poll the members of the `#lunch_buddies` channel. ' +
+  '\n\n' +
+  'It is possible to have multiple polls going at the same time; they are uniquely defined by the channel from which they were created. '
+)
+
+CLOSE_POLL = (
+  'Once everyone has answered, go back to the same channel from which the poll was created ' +
+  'and write `@Lunch Buddies close`. This will randomly group the responders by their answer ' +
+  'and start a private group message for each group. ' +
+  '\n\n' +
+  'If you created the poll with the slash command `/lunch_buddies_create`, you should close it with the slash command `/lunch_buddies_close`.'
+)
+
+SUPPORT_LINE = 'If you have any questions, comments or concerns, please send an email to support@lunchbuddiesapp.com'
 
 APP_EXPLANATION = '{}\n\n{}\n\n{}\n\n{}'.format(
   TAG_LINE,
---
+++ lunch_buddies/scripts/generate_app_store_content.py
@@ -0,0 +1,8 @@
+from lunch_buddies.constants.help import TAG_LINE_SHORT, APP_EXPLANATION
+
+print('** App name **')
+print('Lunch Buddies')
+print('\n\n** Short description **')
+print(TAG_LINE_SHORT)
+print('\n\n** Long description **')
+print(APP_EXPLANATION)
