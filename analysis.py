668b1d992b9cd0d85947f2e52d74076612c8d285
quinn <quinn@quinnweber.com>
2019-07-10 21:18:53 -0700

    do an auth.test

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 
+from lunch_buddies.clients.slack import SlackClient
 from lunch_buddies.dao.poll_responses import PollResponsesDao
 from lunch_buddies.dao.polls import PollsDao
 from lunch_buddies.dao.teams import TeamsDao
@@ -9,6 +10,7 @@ polls_dao = PollsDao()
 polls = polls_dao.read()
 teams_dao = TeamsDao()
 teams = teams_dao.read()
+slack_client = SlackClient()
 
 polls_by_team: dict = defaultdict(list)
 
@@ -25,9 +27,15 @@ def team_summary(team_id, polls):
         'earliest': min(polls, key=lambda x: x.created_at).created_at,
         'latest': max(polls, key=lambda x: x.created_at).created_at,
         'foo': max(polls, key=lambda x: x.created_at),
+        'error': slack_client._get_base_client_for_team(team.bot_access_token).api_call('auth.test').get('error')
     }
 
 
 agg = {k: team_summary(k, v) for k, v in polls_by_team.items()}
 
 sorted(agg.items(), key=lambda v: v[1]['latest'])
+
+{
+    v['name']: v['error']
+    for k, v in agg.items()
+}
