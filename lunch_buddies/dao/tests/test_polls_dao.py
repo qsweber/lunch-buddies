60bae0fc046a8d6351c2c9ffaf683b623af3c34f
quinn <quinn@quinnweber.com>
2020-04-16 09:22:13 -0700

    add txt report

--- lunch_buddies/dao/tests/test_polls_dao.py
+++ lunch_buddies/dao/tests/test_polls_dao.py
@@ -4,7 +4,6 @@ import pytest
 
 from lunch_buddies.lib.service_context import service_context
 from lunch_buddies.models.polls import Poll, Choice
-from lunch_buddies.actions.create_poll import parse_message_text
 
 
 @pytest.mark.parametrize(
@@ -92,42 +91,88 @@ def test_roundtrip(model, dynamo):
     assert from_dynamo.channel_id == model.channel_id
 
 
-def test_handles_missing_values_in_dynamo(mocker):
-    created_at = 1522117983.551714
-
-    mocker.patch.object(
-        service_context.daos.polls,
-        '_read_internal',
-        auto_spec=True,
-        return_value=[
+@pytest.mark.parametrize(
+    'dynamo, expected',
+    [
+        (
             {
                 'team_id': '123',
                 'created_at': 1522117983.551714,
-                # 'channel_id': 'test_channel_id',
+                'channel_id': 'test_channel_id',
                 'created_by_user_id': '456',
                 'callback_id': 'f0d101f9-9aaa-4899-85c8-aa0a2dbb0aaa',
                 'state': 'CREATED',
                 'choices': '[{"key": "yes_1200", "is_yes": true, "time": "12:00", "display_text": "Yes (12:00)"}, {"key": "no", "is_yes": false, "time": "", "display_text": "No"}]',
                 'group_size': 6,
             },
-        ]
+            Poll(
+                team_id='123',
+                created_at=datetime.fromtimestamp(1522117983.551714),
+                channel_id='test_channel_id',
+                created_by_user_id='456',
+                callback_id=uuid.UUID('f0d101f9-9aaa-4899-85c8-aa0a2dbb0aaa'),
+                state='CREATED',
+                choices=[
+                    Choice(
+                        key='yes_1200',
+                        is_yes=True,
+                        time='12:00',
+                        display_text='Yes (12:00)',
+                    ),
+                    Choice(
+                        key='no',
+                        is_yes=False,
+                        time='',
+                        display_text='No',
+                    ),
+                ],
+                group_size=6,
+            ),
+        ),
+        (
+            {
+                'team_id': '123',
+                'created_at': 1522117983.551714,
+                'created_by_user_id': '456',
+                'callback_id': 'f0d101f9-9aaa-4899-85c8-aa0a2dbb0aaa',
+                'state': 'CREATED',
+                'choices': '[{"key": "yes_1200", "is_yes": true, "time": "12:00", "display_text": "Yes (12:00)"}, {"key": "no", "is_yes": false, "time": "", "display_text": "No"}]',
+                'group_size': 6,
+            },
+            Poll(
+                team_id='123',
+                created_at=datetime.fromtimestamp(1522117983.551714),
+                channel_id=None,
+                created_by_user_id='456',
+                callback_id=uuid.UUID('f0d101f9-9aaa-4899-85c8-aa0a2dbb0aaa'),
+                state='CREATED',
+                choices=[
+                    Choice(
+                        key='yes_1200',
+                        is_yes=True,
+                        time='12:00',
+                        display_text='Yes (12:00)',
+                    ),
+                    Choice(
+                        key='no',
+                        is_yes=False,
+                        time='',
+                        display_text='No',
+                    ),
+                ],
+                group_size=6,
+            ),
+        )
+    ]
+)
+def test_from_dynamo(mocker, dynamo, expected):
+    mocker.patch.object(
+        service_context.daos.polls,
+        '_read_internal',
+        auto_spec=True,
+        return_value=[dynamo]
     )
 
-    polls = service_context.daos.polls.read(None, None)
+    poll = service_context.daos.polls.find_by_callback_id_or_die('123', expected.callback_id)
 
-    choices, group_size = parse_message_text('1200')
-
-    expected_polls = [
-        Poll(
-            team_id='123',
-            created_at=datetime.fromtimestamp(created_at),
-            channel_id=None,
-            created_by_user_id='456',
-            callback_id=uuid.UUID('f0d101f9-9aaa-4899-85c8-aa0a2dbb0aaa'),
-            state='CREATED',
-            choices=choices,
-            group_size=6,
-        ),
-    ]
-
-    assert polls == expected_polls
+    assert poll == expected
