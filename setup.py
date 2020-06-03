aa4f10996888169d3d60d6a34655940d12d3f85d
quinn <quinn@quinnweber.com>
2020-06-03 16:00:36 -0700

    add to tox

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='lunch-buddies',
-    version='0.0.2',
-    description='Lunch buddies',
-    author='Quinn Weber',
-    maintainer='Quinn Weber',
-    maintainer_email='quinn@quinnweber.com',
-    packages=find_packages(exclude=('tests',)),
-    install_requires=(
-        'flask',
-        'boto3',
-    ),
+    name="lunch-buddies",
+    version="0.0.2",
+    description="Lunch buddies",
+    author="Quinn Weber",
+    maintainer="Quinn Weber",
+    maintainer_email="quinn@quinnweber.com",
+    packages=find_packages(exclude=("tests",)),
+    install_requires=("flask", "boto3",),
 )
