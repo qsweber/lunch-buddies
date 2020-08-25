7544031a6023ac6dcd0862e3ceacc56d1be0119f
Quinn Weber <quinn@quinnweber.com>
2020-08-24 20:49:56 -0700

    add dry run flag on invoicing

--- tests/actions/test_invoice.py
+++ tests/actions/test_invoice.py
@@ -71,7 +71,7 @@ def mocked_data(mocker):
 
 
 def test_invoice(mocker, mocked_data):
-    module.invoice(service_context)
+    module.invoice(service_context, False)
 
     service_context.clients.stripe.create_invoice.assert_called_with(
         Customer(id=team.stripe_customer_id,),
@@ -97,7 +97,7 @@ def test_invoice_when_not_first_invoice(mocker, mocked_data):
         return_value=Invoice(id="previous", created_at=datetime(2020, 2, 14),),
     )
 
-    module.invoice(service_context)
+    module.invoice(service_context, False)
 
     service_context.clients.stripe.create_invoice.assert_called_with(
         Customer(id=team.stripe_customer_id,),
